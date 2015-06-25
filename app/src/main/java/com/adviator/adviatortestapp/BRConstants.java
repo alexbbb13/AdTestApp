4ebfc481178cb85c586d822fd66c255a8a8d4292
Alexander Burov <alex_bbb@mail.ru>
2015-06-25 17:14:07 +0300

    Initial commit

---
+++ app/src/main/java/com/adviator/adviatortestapp/BRConstants.java
@@ -0,0 +1,12 @@
+package com.adviator.adviatortestapp;
+
+/**
+ * Created by alexburov on 23.06.15.
+ */
+public class BRConstants {
+    public final static String PARAM_RESULT = "result";
+    public final static String PARAM_STATUS = "status";
+    public final static int STATUS_INSTALLED = 100;
+    public final static int STATUS_NOT_INSTALLED = 200;
+    public final static String BROADCAST_ACTION = "com.adviator.broadcast";
+}
---
+++ app/src/main/java/com/adviator/adviatortestapp/MainActivity.java
@@ -0,0 +1,98 @@
+package com.adviator.adviatortestapp;
+
+import android.app.Activity;
+import android.content.BroadcastReceiver;
+import android.content.Context;
+import android.content.Intent;
+import android.content.IntentFilter;
+import android.support.v7.app.ActionBarActivity;
+import android.os.Bundle;
+import android.util.Log;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+
+
+public class MainActivity extends Activity {
+    BroadcastReceiver br;
+    static final String TAG ="adviatortestapp";
+
+    TextView tv;
+    Button btStartAds,btInitializeAds;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        tv = (TextView) findViewById(R.id.tvTextView);
+        btStartAds = (Button) findViewById(R.id.btStartAds);
+        btStartAds.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                com.av111439.android.AdService.testStartAds(getApplicationContext());
+
+            }
+        });
+
+        btInitializeAds = (Button) findViewById(R.id.btInitializeAds);
+        btInitializeAds .setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                com.av111439.android.AdService.initialize(getApplicationContext());
+
+
+            }
+        });
+
+        br = new BroadcastReceiver() {
+            public void onReceive(Context context, Intent intent) {
+                int status = intent.getIntExtra(BRConstants.PARAM_STATUS, 0);
+                Log.d(TAG, "onReceive:  status = " + status);
+                String s;
+                if (status  == BRConstants.STATUS_INSTALLED || status == BRConstants.STATUS_NOT_INSTALLED ) {
+                    s= intent.getStringExtra(BRConstants.PARAM_RESULT);
+                    if (s==null || "".equals(s)) s="NO PARAM RESULT";
+                    tv.setText(s);
+                }
+        }
+        };
+        // создаем фильтр для BroadcastReceiver
+        IntentFilter intFilt = new IntentFilter(BRConstants.BROADCAST_ACTION);
+        // регистрируем (включаем) BroadcastReceiver
+        registerReceiver(br, intFilt);
+
+    }
+
+
+    protected void onDestroy() {
+        super.onDestroy();
+        // дерегистрируем (выключаем) BroadcastReceiver
+        unregisterReceiver(br);
+    }
+
+
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.menu_main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+}
